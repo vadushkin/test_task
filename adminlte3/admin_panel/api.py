5c0488edb3e5543fe29f9ffdadb3cced7f263ff2
vadim-shalapugin <vadimsalapugin@gmail.com>
2022-08-31 12:16:00 +0300

    added sort by created at and pagination

--- adminlte3/admin_panel/api.py
+++ adminlte3/admin_panel/api.py
@@ -1,11 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.pagination import CursorPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from admin_panel.models import News
 from admin_panel.serializers import NewsSerializer
 
 
+class CursorSetPagination(CursorPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    ordering = '-created_at'
+
+
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = CursorSetPagination
