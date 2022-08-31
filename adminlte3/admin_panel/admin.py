5d2df764f2523807a7948dc4d538556632de0580
vadim-shalapugin <vadimsalapugin@gmail.com>
2022-08-31 12:35:10 +0300

    updated admin panel

--- adminlte3/admin_panel/admin.py
+++ adminlte3/admin_panel/admin.py
@@ -4,18 +4,19 @@ from . import models
 
 
 class NewsAdminModel(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'created_at')
     list_filter = ('name',)
     search_fields = ('name', 'description')
     fields = ('name',
               'description',
+              'tags',
               'created_at',
               )
-    readonly_fields = ('created_at',)
 
 
 class TagAdminModel(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',), }
 
 
-admin.site.register(models.News)
+admin.site.register(models.News, NewsAdminModel)
 admin.site.register(models.Tag, TagAdminModel)
